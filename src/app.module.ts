31bc6d183c9c02b28b76cb0b43a6f64e7a676cfc
Nicolae Basso <nickbassotrue@gmail.com>
2021-06-15 01:04:39 +0300

    init GQL communication with MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { GraphQLModule } from '@nestjs/graphql';
 import { LessonModule } from './lesson/lesson.module';
+import { Lesson } from './lesson/lesson.entity';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: 'mongodb+srv://NicolaeAdmin:oUzOcfYkhUK8N4pT@previewcluster.owarn.mongodb.net/polyteia?retryWrites=true&w=majority',
+      synchronize: true,
+      useUnifiedTopology: true,
+      entities: [Lesson],
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
     }),
